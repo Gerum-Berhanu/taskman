d6c8a1cdc91a60df2db271bab07d045103f78b8d
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-08-06 00:09:01 +0300

    feat: add read task endpoint with error handling

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,13 +1,26 @@
 """Tasks HTTP endpoints."""
 
-from fastapi import APIRouter
-from starlette.status import HTTP_201_CREATED
+from fastapi import APIRouter, HTTPException
+from pydantic import UUID4
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
 from app.schemas import TaskCreate, TaskRead
 import app.storage as st
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
+
 @router.post("", response_model=TaskRead, status_code=HTTP_201_CREATED)
 async def create_task(new_task: TaskCreate) -> dict:
     return st.create(new_task.model_dump())
+
+
+@router.get("/{task_id}", response_model=TaskRead)
+async def read_task_by_id(task_id: UUID4) -> dict:
+    task = st.get(task_id)
+    if task is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail=f"Task not found"
+        )
+    return task
