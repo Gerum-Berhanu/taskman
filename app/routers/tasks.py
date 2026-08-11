7d7171a060923881c6e253c548a2d860d53f0e95
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-08-11 20:21:34 +0300

    refactor: simplify task retrieval by using dependency injection

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,6 +1,7 @@
 """Tasks HTTP endpoints."""
 
-from fastapi import APIRouter, HTTPException
+from typing import Annotated, Any
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import UUID4
 from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND
 
@@ -9,7 +10,7 @@ import app.storage as st
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
-def get_task_or_404(task_id: UUID4):
+def get_task_or_404(task_id: UUID4) -> dict:
     task = st.get(task_id)
     if task is None:
         raise HTTPException(
@@ -18,6 +19,8 @@ def get_task_or_404(task_id: UUID4):
         )
     return task
 
+TaskDep = Annotated[dict[str, Any], Depends(get_task_or_404)]
+
 
 @router.post("", response_model=TaskRead, status_code=HTTP_201_CREATED)
 async def create_task(new_task: TaskCreate) -> dict:
@@ -25,8 +28,8 @@ async def create_task(new_task: TaskCreate) -> dict:
 
 
 @router.get("/{task_id}", response_model=TaskRead)
-async def read_task_by_id(task_id: UUID4) -> dict:
-    return get_task_or_404(task_id)
+async def read_task_by_id(task: TaskDep) -> dict:
+    return task
 
 
 @router.get("", response_model=list[TaskRead])
@@ -35,12 +38,10 @@ async def read_all_tasks() -> list[dict]:
 
 
 @router.patch("/{task_id}", response_model=TaskRead)
-async def update_task(task_id: UUID4, task_in: TaskUpdate) -> dict:
-    get_task_or_404(task_id)
-    return st.update(task_id, task_in.model_dump(exclude_unset=True))
+async def update_task(task: TaskDep, task_in: TaskUpdate) -> dict:
+    return st.update(task["id"], task_in.model_dump(exclude_unset=True))
 
 
 @router.delete("/{task_id}", status_code=HTTP_204_NO_CONTENT)
-async def delete_task(task_id: UUID4):
-    get_task_or_404(task_id)
-    st.delete(task_id)
+async def delete_task(task: TaskDep):
+    st.delete(task["id"])
