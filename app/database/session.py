407c03f7d7c103e6d0bdf5c51dd40f2522b66b81
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-08-24 11:01:29 +0300

    refactor: simplify connection arguments in session configuration

--- app/database/session.py
+++ app/database/session.py
@@ -5,9 +5,7 @@ from sqlmodel import SQLModel, create_engine
 from app.core.config import settings
 import app.database.models # run the class bodies/definitions so registration happens
 
-connect_args = (
-    {"check_same_thread": False} # sqlite only, postgres doesn't need check_same_thread
-)
+connect_args = {"check_same_thread": False} # sqlite only, postgres doesn't need check_same_thread
 
 engine = create_engine(url=settings.database_url, connect_args=connect_args)
 
