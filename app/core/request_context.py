e1d861bb448acc00add9a3ae13da27a0d3246d34
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-09-26 10:55:41 +0300

    feat: add actor_id in logs of task and workspace member actions

--- app/core/request_context.py
+++ app/core/request_context.py
@@ -1,5 +1,4 @@
 from contextvars import ContextVar
 
 request_id_ctx: ContextVar[str] = ContextVar("request_id", default="-")
-
-
+current_user_id_ctx: ContextVar[str] = ContextVar[str]("current_user_id", default="-")
